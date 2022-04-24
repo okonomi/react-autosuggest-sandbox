d64b20f4feaf6819b3e426d1f95888e27c6c53b5
okonomi <okonomi@oknm.jp>
2022-04-24 14:31:12 +0900

    introduce react

---
+++ app/javascript/suggest.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+const container = document.getElementById("app")
+
+const root = createRoot(container)
+
+root.render(<p>suggest</p>)
