6d1b071857aeccd7bc1710188e4b04ba6086c8bc
okonomi <okonomi@oknm.jp>
2022-04-24 14:52:48 +0900

    add example

--- app/javascript/suggest.tsx
+++ app/javascript/suggest.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 
-import Suggest from "./components/Suggest"
+// import Suggest from "./components/Suggest"
+import AutosuggestExample from "./components/AutosuggestExample"
 
 const container = document.getElementById("app")
 const root = createRoot(container)
 
-root.render(<Suggest />)
+root.render(<AutosuggestExample />)
