0ce3607b1d392cd98257fbebd7b9d83f7d39c74b
okonomi <okonomi@oknm.jp>
2022-04-24 19:04:43 +0900

    limit

--- app/controllers/local_governments_controller.rb
+++ app/controllers/local_governments_controller.rb
@@ -1,6 +1,6 @@
 class LocalGovernmentsController < ApplicationController
   def index
     @q = LocalGovernment.ransack(params[:q])
-    @local_governments = @q.result
+    @local_governments = @q.result.limit(25)
   end
 end
